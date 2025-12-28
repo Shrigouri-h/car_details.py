3459440e68ee709e7af8534114a44e7ae41f6e8f
Shrigouri-h <shrigouri0707@gmail.com>
2025-12-28 10:33:18 +0530

    Update car_details.py

--- car_details.py
+++ car_details.py
@@ -1,8 +1,6 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 
-
-
 class Car:
     def __init__(self, brand, model, speed, mileage, price):
         self.brand = brand
@@ -17,15 +15,32 @@ class Car:
         print(f"Model   : {self.model}")
         print(f"Speed   : {self.speed} km/h")
         print(f"Mileage : {self.mileage} km/l")
-        print(f"Price   : {self.price}")
+        print(f"Price   : ₹{self.price}")
 
 
 def main():
-    if len(sys.argv) != 6:
-        print("Usage: python car_details.py <brand> <model> <speed> <mileage> <price>")
-        sys.exit(1)
+    script_name = sys.argv[0]
 
-    _, brand, model, speed, mileage, price = sys.argv
+    # ✅ Default values in code
+    brand = "HONDA"
+    model = "CITY"
+    speed = 200
+    mileage = 10
+    price = 5000000
+
+    # ✅ If Jenkins provides parameters, override defaults
+    if len(sys.argv) == 6:
+        print("Using values provided by Jenkins parameters:\n")
+        brand = sys.argv[1]
+        model = sys.argv[2]
+        speed = sys.argv[3]
+        mileage = sys.argv[4]
+        price = sys.argv[5]
+    else:
+        print("No Jenkins parameters given - using default values from code:\n")
+
+    print("Script Name:", script_name)
+
     car = Car(brand, model, speed, mileage, price)
     car.display()
 
